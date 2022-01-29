2d2af4d60c3067fcdb47cb731f328820b5bf91d2
qiaoyang04 <qiaoyang04@meituan.com>
2022-01-29 18:30:45 +0800

    feat: 509 pro

--- Array/easy/509-斐波那契数列/509-fib.js
+++ Array/easy/509-斐波那契数列/509-fib.js
@@ -3,18 +3,17 @@
  * Date: 2022-1-29
  * me: 斐波那契数列数列：要求至少2种解法：
  * 1. 递归：自顶向下
- * 2. 迭代：自底向上
+ * 2. 迭代：自底向上 + 状态压缩 （面试官肯定期望简单问题能给出尽可能优的解）
  */
 
 /**
- * 法1: 递归解法
+ * 法1: 递归解法：自顶向下
  * @param {number} n
  * @return {number}
  */
 var fib = function(n) {
   // 防御
   if (typeof n !== 'number') return;
-  if (n === 0) return [0];
   // 初始化值
   // 核心算法
   // 边界
@@ -24,4 +23,49 @@ var fib = function(n) {
   return fib(n - 1) + fib(n - 2);
 };
 
-module.exports = fib;
+/**
+ * 法2: 迭代解法：自底向上
+ * @param {number} n
+ * @return {number}
+ */
+ var fib2 = function(n) {
+   // 防御
+   if (typeof n !== 'number') return;
+   // 初始化变量
+   const dp = [];
+   dp[0] = 0;
+   dp[1] = 1;
+   // 遍历
+   for (let i = 2; i <= n; i++) {
+     dp[i] = dp[i-1] + dp[i-2];
+   }
+   // 返回结果
+   return dp[n];
+ }
+
+ /**
+ * 法2+1: 迭代解法：自底向上
+ * 加入状态压缩
+ * @param {number} n
+ * @return {number}
+ */
+  var fib3 = function(n) {
+    // 防御
+    if (typeof n !== 'number') return;
+    // base case
+    if (n <= 1) return n;
+    // 初始化变量
+    let n2 = 0; 
+    let n1 = 1;
+    let res = 1;
+    // 遍历
+    for (let i = 2; i <= n; i++) {
+      res = n1 + n2;
+      n2 = n1;
+      n1 = res;
+    }
+    // 返回结果
+    return res;
+  }
+
+module.exports = fib3;
---
+++ Array/easy/509-斐波那契数列/__509-fib.test.js
@@ -0,0 +1,5 @@
+const fib = require('./509-fib');
+
+test('fib-1', () => {
+  expect(fib(3)).toBe(2);
+});
