9718a6aee89bcf8e1ef4946e9029e0a2a69949c9
qiaoyang04 <qiaoyang04@meituan.com>
2022-02-02 18:10:23 +0800

    feat: 21

--- Linked-list/easy/21-合并两个有序链表/21-mergeTwoLists.js
+++ Linked-list/easy/21-合并两个有序链表/21-mergeTwoLists.js
@@ -18,6 +18,36 @@
  */
 var mergeTwoLists = function(list1, list2) {
   // 防御
+  // 初始值
+  // 建一个虚拟头节点 避免单独处理头结点等边界情况
+  const dummyHead = new ListNode();
+  let p = dummyHead;
+  let p1 = list1, p2 = list2;
+  // 核心算法
+  while (p1 !== null && p2 !== null) {
+    if (p1.val < p2.val) {
+      p.next = p1;
+      p1 = p1.next;
+    } else {
+      p.next = p2;
+      p2 = p2.next;
+    }
+
+    // 指针前进
+    p = p.next;
+  }
+
+  // 未遍历完的链表的部分 可以直接拼接在p的后面
+  if (p1 !== null) {
+      p.next = p1;
+  }
+
+  if (p2 !== null) {
+      p.next = p2;
+  }
+
+  // 返回结果 （虚拟头结点本身是不存任何有效数据的，所以真的链表都是从dummy的next开始的 dummy.next才是真正的head）
+  return dummyHead.next; 
 };
 
 
