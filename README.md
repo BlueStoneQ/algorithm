ae45b8aa0da4c4509d73d642731c40bf257787cb
qiaoyang04 <qiaoyang04@meituan.com>
2022-02-04 21:51:21 +0800

    feat: 116

--- README.md
+++ README.md
@@ -19,6 +19,9 @@ A record of algorithm practice.
 - [144-二叉树的前序遍历](Tree/easy/144-二叉树的前序遍历/144-preorderTraversal.js)
 - [543-二叉树的直径](Tree/easy/543-二叉树的直径/543-diameterOfBinaryTree.js)
 - [226-翻转二叉树](Tree/easy/226-翻转二叉树/226-invertTree.js)
+### 层序遍历BFS
+#### medium
+- [116-填充每个节点的下一个右侧节点指针](Tree/medium/116-填充每个节点的下一个右侧节点指针/116-connect.js)
 ## 链表
 ```
 注意：链表中的虚拟头结点 是一个很重要的技巧, 用来防止很多边界错误
---
+++ Tree/medium/116-填充每个节点的下一个右侧节点指针/116-connect.js
@@ -0,0 +1,56 @@
+/**
+ * leet: https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+ * Date: 2022-2-4
+ * dong: https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247487126&idx=1&sn=4de13e66397bc35970963c5a1330ce18&scene=21#wechat_redirect
+ * 寻找最正统的解法：
+ *  - 这里选择官方题解：BFS方法：
+ *    - https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/solution/tian-chong-mei-ge-jie-dian-de-xia-yi-ge-you-ce-2-4/
+ */
+
+
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * 使用BFS：进行层序遍历
+ * @param {Node} root
+ * @return {Node}
+ */
+ var connect = function(root) {
+  // 防御
+  if (root === null) return root;
+  // 定义BFS需要的辅助数据结构：队列
+  const queue = [root];
+  // 开始BFS
+  while (queue.length > 0) {
+      // 因为队列后面不断有子节点加入 所以 进入一层的时候 记录下当前层元素的个数
+      const layerLength = queue.length;
+      // 遍历当前这一层
+      for (let i = 0; i < layerLength; i++) {
+          const cur = queue.shift();
+
+          // 链接左右节点 边界：该层的末尾 是不连接的
+          if (i < layerLength - 1) {
+              cur.next = queue[0];
+          }
+
+          // 不断将下面一层的子节点加入队列
+          if (cur.left !== null) {
+              queue.push(cur.left);
+          }
+          if (cur.right !== null) {
+              queue.push(cur.right);
+          }
+      }
+  }
+  return root;
+};
+
+module.exports = connect;
