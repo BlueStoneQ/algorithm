5beffbec1a63b61570d48f7a65ba3798058e6894
qiaoyang04 <qiaoyang04@meituan.com>
2022-02-04 19:56:19 +0800

    feat: 144

---
+++ Tree/easy/144-二叉树的前序遍历/144-preorderTraversal.js
@@ -0,0 +1,39 @@
+/**
+ * leet: https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+ * Date: 2022-2-4
+ * dong: https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247494655&idx=1&sn=f3445112d7322ea8491073fd2d19f25c&scene=21#wechat_redirect
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var preorderTraversal = function(root) {
+  // 初始化变量
+  let res = [];
+  // 定义辅助遍历函数: 单纯递归遍历
+  const _traverse = function(cur) {
+      // base case
+      if (cur === null) {
+          return;
+      }
+      res.push(cur.val);
+      // 递归遍历
+      _traverse(cur.left);
+      _traverse(cur.right);
+  }
+  // 调用辅助遍历函数
+  _traverse(root);
+  // 返回结果
+  return res;
+};
+
+module.exports = preorderTraversal;
