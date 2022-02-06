d86b9889357aafb510c8ce7ac667ec98d50f4725
qiaoyang04 <qiaoyang04@meituan.com>
2022-02-06 19:13:49 +0800

    feat: 297

--- README.md
+++ README.md
@@ -40,6 +40,7 @@ A record of algorithm practice.
 - [652-寻找重复的子树](Tree/medium/652-寻找重复的子树/652-findDuplicateSubtrees.js)
 #### hard
 - [297-二叉树的序列化与反序列化-后序遍历法](Tree/297-二叉树的序列化与反序列化/297-serialize-DFS-postorder.js)
+- [297-二叉树的序列化与反序列化-前序遍历法](Tree/297-二叉树的序列化与反序列化/297-serialize-DFS-preorder.js)
 ### BST 与 中序遍历
 #### easy
 - [700-二叉搜索树中的搜索](Tree/easy/700-二叉搜索树中的搜索/700-searchBST.js)
--- Tree/297-二叉树的序列化与反序列化/297-serialize-DFS-preorder.js
+++ Tree/297-二叉树的序列化与反序列化/297-serialize-DFS-preorder.js
@@ -10,7 +10,7 @@
  const NULL = '#';
  
  /**
-  * 方法1： DFS-后序遍历
+  * 方法1： DFS-前序遍历
   * Encodes a tree to a single string.
   * null = '#', 树与树 以及 root 之间用','来分隔
   * @param {TreeNode} root
@@ -25,18 +25,19 @@
              res += NULL + SEP;
              return;
          };
+ 
+         // 序列化当前节点
+         res +=  curRoot.val + SEP;
+ 
          // 调用递归
          _serialize(curRoot.left);
          _serialize(curRoot.right);
-         // 序列完左右子树后 序列化当前节点
-         res +=  curRoot.val + SEP;
      }
  
      // 调用辅助递归函数
      _serialize(root);
  
-     // 这里的序列化 会给后面多加一个SEP 可以修剪掉 保证后面反序列化程序的平滑
-     return res.substring(0, res.length - 1);
+     return res;
  };
  
  
@@ -56,15 +57,16 @@
  function _deserialize(nodes) {
      // base case
      if (nodes.length === 0) return null;
-     // 找到当前的根节点：后续遍历的根节点为最后一个元素，处理过后的元素 需要排出序列
-     const curRootVal = nodes.pop();
+     // 找到当前的根节点：前续遍历的根节点为第一个元素，处理过后的元素 需要排出序列
+     const curRootVal = nodes.shift();
      // 判断当前节点是否为空节点
      if (curRootVal === NULL) return null;
      // 构造当前root节点
      const curRoot = new TreeNode(+curRootVal); // 记得转变为数字再存入Tree中
-     // 递归调用 用剩余的序列 获得左右子树: 后续序列的特点：left - right - root，目前从后向前遍历序列，所以，接下来遍历的是右子树 然后是左子树
-     const right = _deserialize(nodes);
+     // 递归调用 用剩余的序列 获得左右子树: 
+     // 前续序列的特点：right - left - root，目前从前向后遍历序列，所以，接下来遍历的是left子树 然后是right子树
      const left = _deserialize(nodes);
+     const right = _deserialize(nodes);
      // 挂载左右子树到当前节点上
      curRoot.left = left;
      curRoot.right = right;
