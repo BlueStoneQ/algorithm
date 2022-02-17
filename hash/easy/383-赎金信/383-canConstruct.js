6bf2700b913b96951ef3ce27257baab8e322bb4a
qiaoyang04 <qiaoyang04@meituan.com>
2022-02-17 21:13:27 +0800

    feat: 383

--- hash/easy/383-赎金信/383-canConstruct.js
+++ hash/easy/383-赎金信/383-canConstruct.js
@@ -5,6 +5,7 @@
  */
 
 /**
+ * 方法1： 使用map来作查找表 但是 底层消耗可能会较高
  * me: 这道题很像异位词 但是其实是判断 magzine 是不是 ransomNote的超集，也就是说数量不要求相等
  * 我们依然先构建要查找的目标字符串的map, 并统计其每个字符出现的次数（可以理解为一种编码）
  * 然后再遍历源字符串 用这个map去查 每查到一个合法字符 就把这个字符数量记录减一，如果map该数量减到了0 则该字符可以delete掉,当然 如果遍历中发现map为空 也可以提前return true掉
@@ -34,4 +35,30 @@
   }
   // return 
   return ransomNoteMap.size === 0;
+};
+
+
+/**
+ * 方法2：Array + ascll构建hash作为查找表
+ * 字符串 尤其是强调小写的字符串 则使用数组+ascll码建立hash
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+ var canConstruct = function(ransomNote, magazine) {
+  // defend
+  // init data
+  const charArr = new Array(26).fill(0); // 26个小写字母的ascll码刚好映射出26个位置
+  const baseAscll = 'a'.charCodeAt();
+  for (const mChar of magazine) {
+      charArr[mChar.charCodeAt() - baseAscll]++;
+  }
+  // algo
+  for (const rChar of ransomNote) {
+      const curIndex = rChar.charCodeAt() - baseAscll;
+      if (charArr[curIndex] === 0) return false;
+      charArr[curIndex]--;
+  }
+  // return 
+  return true;
 };
