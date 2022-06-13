01a4009db5a7bba135db1fabea650402a6aeffb2
qiaoyang <2282907674@qq.com>
2022-06-13 16:08:34 +0800

    feat: 77

--- DFS/medium/77-组合/77-combine.js
+++ DFS/medium/77-组合/77-combine.js
@@ -4,7 +4,50 @@
  * https://programmercarl.com/0077.%E7%BB%84%E5%90%88.html#javascript
  */
 
+/**
+ * 以该版为参考：
+ * DFS+startIndex 这是解决组合问题的通用模版
+ * 
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+// defend
+// init data
+const res = [];
+// algo
+
+// 1. 回溯函数的定义 入参 返回值
+const _combine = (path, startIndex) => {
+    // 2. base case: k其实就是多叉树的深度
+    if (path.length === k) {
+        res.push(path.slice());
+        return;
+    }
+
+    // 遍历当前层
+    // 注意：排列这里i就是0开始，组合 是从startIndex开始的
+    for (let i = startIndex; i <= n; i++) {
+        // todo: 剪枝 去重
+        // 做出选择
+        path.push(i);
+        // 递归回溯
+        _combine(path, i + 1);
+        // 撤销选择
+        path.pop();
+    }
+}
+
+_combine([], 1);
+// return 
+return res;
+};
+
+
  /**
+  * 
+  * 初版方法：通用性不强，性能不优-频繁slice 应该采用更通用的startIndex来标记状态
  * @param {number} n
  * @param {number} k
  * @return {number[][]}
