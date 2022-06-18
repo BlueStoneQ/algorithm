34603d02c7aedeb404d80c7a572e8a9d61210010
qiaoyang <2282907674@qq.com>
2022-06-18 17:01:08 +0800

    fix: 746

--- DP/基础题目/easy/746-使用最小花费爬楼梯/746-minCostClimbingStairs.js
+++ DP/基础题目/easy/746-使用最小花费爬楼梯/746-minCostClimbingStairs.js
@@ -11,6 +11,9 @@
  * dp数组初始化：dp[0] = cost[0]; dp[0] = cost[1];
  * 遍历顺序-根据转移公式确定：前 -> 后
  * 距离dp数组：dp(5) = [1, 100, 2, 3, 3]
+ * 
+ * 这里按照官方的题解为准：比较能解释等通：
+ * https://leetcode.cn/problems/min-cost-climbing-stairs/solution/shi-yong-zui-xiao-hua-fei-pa-lou-ti-by-l-ncf8/
  * @param {number[]} cost
  * @return {number}
  */
@@ -19,17 +22,16 @@
   if (cost.length === 0) return 0;
   if (cost.length === 1) return cost[0];
   // init data
-  const costLen = cost.length;
+  const n = cost.length;
   const dp = [];
-  dp[0] = cost[0];
-  dp[1] = cost[1];
-  // algo
-  for (let i = 2; i < costLen; i++) {
-      dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+  // 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。 - 因为我们要求最小的 所以 选择从1开始爬
+  dp[0] = 0; 
+  dp[1] = 0;
+
+  for (let i = 2; i <= n; i++) {
+      // 因为每次可以爬1-2 所以 有2种方案爬到当前i
+      dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
   }
-  // return 取到达倒数第一步 倒数第二部花费的最小值（因为一次可以跳1步 或者 2步）这里注意cost的定义 是从台阶向上爬1-2步所需要的花费 所以达到的最后一步的花费
-  // kaer: 这道题描述也确实有点魔幻。题目描述为：每当你爬上一个阶梯你都要花费对应的体力值，一旦支付了相应的体力值，你就可以选择向上爬一个阶梯或者爬两个阶梯。
-  // 从题目描述可以看出：要不是第一步不需要花费体力，要不就是第最后一步不需要花费体力，我个人理解：题意说的其实是第一步是要支付费用的！。
-  //    因为是当你爬上一个台阶就要花费对应的体力值！所以我定义的dp[i]意思是也是第一步是要花费体力的，最后一步不用花费体力了，因为已经支付了。
-  return Math.min(dp[costLen - 1], dp[costLen - 2]);
+
+  return dp[n];
 };
