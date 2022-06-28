f93119a8d9ab01b21a3c795c1ed15627386024fc
qiaoyang <2282907674@qq.com>
2022-06-28 14:41:04 +0800

    refactor: 232

--- stack&queue/easy/232-用栈实现队列/232-MyQueue.js
+++ stack&queue/easy/232-用栈实现队列/232-MyQueue.js
@@ -4,7 +4,7 @@
  * dong: https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247484567&idx=1&sn=e70930648967a0e4cbf0f594bc9f059d&scene=21#wechat_redirect
  * kaer: https://programmercarl.com/0232.%E7%94%A8%E6%A0%88%E5%AE%9E%E7%8E%B0%E9%98%9F%E5%88%97.html#%E5%85%B6%E4%BB%96%E8%AF%AD%E8%A8%80%E7%89%88%E6%9C%AC
  */
-
+const inStack2OutStack = Symbol('inStack2OutStack');
 
 /**
  * 用2个互逆的栈实现队列
@@ -26,8 +26,8 @@ MyQueue.prototype.push = function(x) {
 * @return {number}
 */
 MyQueue.prototype.pop = function() {
-  // [关键]先调用peek保证stackOut非空
-  this.peek();
+  // [关键]如果stackOut为空 需要把stackIn全部逆序放入到stackIn中
+  this[onStack2OutStack]();
 
   return this.sTackOut.pop();
 };
@@ -37,11 +37,7 @@ MyQueue.prototype.pop = function() {
 */
 MyQueue.prototype.peek = function() {
   // [关键]如果stackOut为空 需要把stackIn全部逆序放入到stackIn中
-  if (this.sTackOut.length === 0) {
-      while (this.sTackIn.length > 0) {
-          this.sTackOut.push(this.sTackIn.pop());
-      }
-  }
+  this[onStack2OutStack]();
   // 读取stackOut的栈顶
   return this.sTackOut[this.sTackOut.length - 1];
 };
@@ -53,6 +49,16 @@ MyQueue.prototype.empty = function() {
   // 2个栈为空 则整个队列就空了 因为该队列是2个栈元素之和
   return this.sTackOut.length === 0 && this.sTackIn.length === 0;
 };
+
+// 将instack注入到outStack
+MyQueue.prototype[onStack2OutStack] = function() {
+  // 在outStack为空的时候，将inStack 导入到 outStack
+  if (this.outStack.length === 0) {
+      while (this.inStack.length > 0) {
+          this.outStack.push(this.inStack.pop());
+      }
+  }
+}
 
 /**
 * Your MyQueue object will be instantiated and called as such:
