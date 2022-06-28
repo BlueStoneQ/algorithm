6fca4fb6c63bd622d6f0ff86f9953b47a15ff59d
qiaoyang <2282907674@qq.com>
2022-06-28 11:10:46 +0800

    refactor: 49

--- hash/medium/49-字母异位词分组/49-groupAnagrams.js
+++ hash/medium/49-字母异位词分组/49-groupAnagrams.js
@@ -4,9 +4,19 @@
  * kaer: https://programmercarl.com/0242.%E6%9C%89%E6%95%88%E7%9A%84%E5%AD%97%E6%AF%8D%E5%BC%82%E4%BD%8D%E8%AF%8D.html#%E7%9B%B8%E5%85%B3%E9%A2%98%E7%9B%AE
  */
 
+(() => {
+  Map.prototype.$addStr = function (key, str) {
+      const originalVal = this.get(key) || [];
+      originalVal.push(str);
+      this.set(key, originalVal);
+  }
+})()
+
 /**
  * dong: 异位词这类问题的关键在于，你如何迅速判断两个字符串是异位词。
  * 一般的思路是对字符串进行编码，编码方式可以是多样的，只要使得异位词的编码结果相同即可。
+ * 
+ * 核心就是：通过编码生成每一个异位词的编码(hash值)作为map的key,这样编码一致的都放在同样的key下 自然就分组了
  * @param {string[]} strs
  * @return {string[][]}
  */
@@ -17,10 +27,8 @@
   const resMap = new Map(); // 字符串编码: 符合该字符串编码的字符串
   // algo
   for (const str of strs) {
-      const curStrCode = encode(str);
-      const curStrArr = resMap.get(curStrCode) || [];
-      curStrArr.push(str)
-      resMap.set(curStrCode, curStrArr);
+      const key = encode(str);
+      resMap.$addStr(key, str);
   }
 
   // 遍历map 生成 Array
